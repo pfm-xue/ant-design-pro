537af388a68a50b19b2e1681676a522858fe4c99
pfm-xue <xuezhuang@pfm-tech.com>
2018-07-31 16:16:10 +0800

    主题颜色

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 module.exports = {
-  'primary-color': '#7c01ff',
+  'primary-color': '#37b991',
   'success-color': '#00baff',
   'card-actions-background': '#f5f8fa',
   'link-color': '#1DA57A',
